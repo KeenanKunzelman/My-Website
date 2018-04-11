87d60857a949ebe246d52343369f6a76a9775b15
KeenanKunzelman <keekunz@gmail.com>
2018-04-11 11:14:56 -0400

    changed up UI and added some content (still needs spell check)

--- javascript.js
+++ javascript.js
@@ -1,39 +1,22 @@
 
+
 $(function() {
-  $('body').scrollspy({ target: '#navbar-example' })
-var contactpos = $("#contact").offset();
-var portfoliopos = $("#portfolio").offset();
-var skillspos = $("#skills").offset();
+
+  var contactpos = $("#contact").offset();
+  var portfoliopos = $("#portfolio").offset();
+  var skillspos = $("#skills").offset();
 
 function scroll(button, positon) {
   $(button).on("click", function(e){
   e.preventDefault();
   $("html, body").animate({ scrollTop: positon}, 'swing');
   });
-
 }
 
-
     scroll("#btn","0px");
-    //
-    // $("#btn").on("click", function(e){
-    // e.preventDefault();
-    // $("html, body").animate({ scrollTop: "0px" }, 'swing');
-    // });
-
-    scroll("#btn2", portfoliopos.top - 56)
-    // $("#btn2").on("click", function(e){
-    // e.preventDefault();
-    //
-    // $("html, body").animate({ scrollTop: (portfoliopos.top - 56) } , 'swing');
-    //
-    // });
-    $("#btn3").on("click", function(e){
-    e.preventDefault();
-
-    $("html, body").animate({ scrollTop: contactpos.top - 56 }, 'swing');
-
-    });
+    scroll("#btn2", contactpos.top - 56);
+    scroll("#btn3",  portfoliopos.top - 56);
+    scroll("#btn4", skillspos.top - 56)
 
 
 });
